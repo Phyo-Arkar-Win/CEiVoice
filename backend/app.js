78769f2b27ff3ade93b2473418fa0c3519ce0464
Phyo Arkar Win <phyoarkar3730@gmail.com>
2026-02-17 23:48:41 +0700

    Ollama Integration Draft

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-// import ollamaRoutes from './routes/ollama.routes.js';
+import ollamaRoutes from './routes/ollama.routes.js';
 import healthCheckRoutes from './routes/healthCheck.routes.js';
 
 const app = express();
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-// app.use('/api/health', healthCheckRoutes);
-// app.use('/api/AI', ollamaRoutes);
+app.use('/api/health', healthCheckRoutes);
+app.use('/api/AI', ollamaRoutes);
 
 export default app;
--- backend/services/ollama.service.js
+++ backend/services/ollama.service.js
@@ -4,14 +4,45 @@ import { Ollama } from 'ollama';
 export const generateDraftTicket = async (req, res) => {
     try {
         const { message } = req.body;
+
+        const prompt = "You are supposed to analyze the content, automatically generating a draft ticket complete with a " +
+        "suggested title, category, summary, and resolution path, which will allow administrators and support staff to focus on " +
+        "resolution rather than categorization and assignment. Return to me in valid JSON format. Escape apostrophes properly. Ensure valid JSON format"+
+        "The keys are\n"+
+        "1. Title: A concise title\n" +
+        "2. Summary: A brief summary of the issue (maximum 4 words)\n" +
+        "3. Category: The category of the issue\n" +
+        "4. Resolution Path: One best suggested way to resolve the issue\n" +
+        "Content: " + message;
+
         const ollama = new Ollama({ host: 'http://localhost:11434' });
-        const response = await ollama.chat({
+
+        const response = await ollama.generate({
             model: "llama3.2",
-            prompt: message,
-            format: "json"
+            prompt,
+            format: "json",
+            options: {
+                temperature: 0.1
+            }
         });
-        res.json(response.data);
+
+
+        const parsed = JSON.parse(response.response);
+        
+        // Issue Message
+        console.log("\nIssue: ", message);
+
+        console.log("\nOllama Raw Response: ", response.response);
+
+        console.log("\nOllama Response")
+        console.log("Title: ", parsed.title);
+        console.log("Summary: ", parsed.summary);
+        console.log("Category: ", parsed.category);
+        console.log("Resolution Path: ", parsed.resolution_path);
+
+        res.json(parsed);
+
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
-}
+};
