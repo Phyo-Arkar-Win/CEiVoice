277651db380919091703e70b8cbca73b6e8b38ee
Phyo Arkar Win <phyoarkar3730@gmail.com>
2026-02-18 13:24:40 +0700

    Ollama Prompt Edit

--- backend/services/ollama.service.js
+++ backend/services/ollama.service.js
@@ -5,15 +5,15 @@ export const generateDraftTicket = async (req, res) => {
     try {
         const { message } = req.body;
 
-        const prompt = "You are supposed to analyze the content, automatically generating a draft ticket complete with a " +
+        const prompt = "You are supposed to analyze the user's request message, automatically generating a draft ticket complete with a " +
         "suggested title, category, summary, and resolution path, which will allow administrators and support staff to focus on " +
         "resolution rather than categorization and assignment. Return to me in valid JSON format. Escape apostrophes properly. Ensure valid JSON format"+
         "The keys are\n"+
-        "1. Title: A concise title\n" +
-        "2. Summary: A brief summary of the issue (maximum 4 words)\n" +
-        "3. Category: The category of the issue\n" +
-        "4. Resolution Path: One best suggested way to resolve the issue\n" +
-        "Content: " + message;
+        "1. title: (Max 100 characters) A concise title. The suggested Title must accurately reflect the core issue described in the user's request message.\n" +
+        "2. summary: (Max 500 characters) The Summary must capture all key facts and the user's ultimate goal mentioned in the user's request message.\n" +
+        "3. category: The category of the request\n" +
+        "4. resolution_path: This suggestion must propose 1-3 actionable steps or resources relevant to the request.\n" +
+        "Request Message: " + message;
 
         const ollama = new Ollama({ host: 'http://localhost:11434' });
 
@@ -28,9 +28,9 @@ export const generateDraftTicket = async (req, res) => {
 
 
         const parsed = JSON.parse(response.response);
-        
+
         // Issue Message
-        console.log("\nIssue: ", message);
+        console.log("\nRequest Message: ", message);
 
         console.log("\nOllama Raw Response: ", response.response);
 
