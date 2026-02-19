dfb1c1d477f3dc59605ef57cc75a688c6b86ad4e
67011617_luu <67011617@kmitl.ac.th>
2026-02-20 01:47:32 +0700

    SUPAnig

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,11 @@ dotenv.config();
 import connectDB from './config/db.js';
 import app from "./app.js";
 
+<<<<<<< HEAD
 const PORT = process.env.PORT;
+=======
+const PORT = process.env.PORT || 5000;
+>>>>>>> 9870920 (SUPAnig)
 
 // Connect to MongoDB
 connectDB();
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState } from 'react'
 
 function App() {
@@ -8,8 +9,31 @@ function App() {
       <h1 class="text-3xl font-bold underline">
         Hello world!
       </h1>
+=======
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+// import Signup from './pages/Signup';
+
+const App = () => {
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Signup />} />
+        {/* <Route path='/register' element={<Signup />} /> */}
+    </Routes>
+    </BrowserRouter>
+>>>>>>> 9870920 (SUPAnig)
     </>
   )
 }
 
+<<<<<<< HEAD
 export default App
+=======
+export default App;
+>>>>>>> 9870920 (SUPAnig)
