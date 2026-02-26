0b0851d3ab5ac0d1033388e2b69ec4a439161d9f
Leur <huangzule0118@gmail.com>
2026-02-27 04:00:31 +0700

    Add custom Tailwind colors

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react'
-import Navbar from '../components/Navbar'
-import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from 'jwt-decode'
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import { useNavigate } from 'react-router';
-import api from "../api/axios"
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { useNavigate } from "react-router";
+import api from "../api/axios";
 
 export default function Login() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [showPassword, setShowPassword] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   // LOGIN
   const handleManualLogin = async (e) => {
@@ -19,7 +19,7 @@ export default function Login() {
     try {
       const response = await api.post("/login", {
         email,
-        password
+        password,
       });
 
       console.log("Login response:", response.data);
@@ -30,19 +30,20 @@ export default function Login() {
 
       // alert(`Welcome, ${user.name}!`);
       navigate("/dashboard");
-    }
-
-    catch (error) {
+    } catch (error) {
       console.error("Login failed:", error);
-      alert(error.response?.data?.message || "Login failed. Please check your credentials.");
+      alert(
+        error.response?.data?.message ||
+          "Login failed. Please check your credentials.",
+      );
     }
-  }
+  };
 
   // GOOGLE LOGIN
   const handleGoogleLoginSuccess = async (credentialResponse) => {
     try {
       const response = await api.post("/login/google", {
-        token: credentialResponse.credential
+        token: credentialResponse.credential,
       });
 
       console.log("Backend response:", response.data);
@@ -53,27 +54,22 @@ export default function Login() {
 
       // alert(`Welcome, ${user.name}!`);
       navigate("/dashboard");
-
     } catch (error) {
       console.error("Google login failed at backend:", error);
       alert("Google login failed. Please try again.");
     }
-  }
+  };
 
   return (
     <>
-      <div className="min-h-screen bg-gray-300">
-
+      <div className="min-h-screen bg-background ">
         {/* Navbar */}
         <Navbar title="Sign up" />
 
         {/* Centered Card */}
         <div className="py-6 flex justify-center items-center mt-16">
-          <div className="bg-[rgb(241,236,236)] w-96 rounded-2xl shadow-lg p-8">
-
-            <h2 className="text-2xl font-bold text-center mb-6">
-              Login
-            </h2>
+          <div className="bg-surface w-96 rounded-2xl shadow-lg p-8">
+            <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
 
             <form onSubmit={handleManualLogin}>
               {/* Email */}
@@ -101,16 +97,16 @@ export default function Login() {
                 <button
                   type="button"
                   onClick={() => setShowPassword(!showPassword)}
-                  className='absolute right-3 top-10 cursor-pointer'
+                  className="absolute right-3 top-10 cursor-pointer"
                 >
-                  {showPassword ? <FaEye /> :<FaEyeSlash /> }
+                  {showPassword ? <FaEye /> : <FaEyeSlash />}
                 </button>
               </div>
 
               {/* Login Button */}
               <button
                 type="submit"
-                className="w-full mt-0.5 bg-[rgb(227,82,5)] hover:bg-[rgb(180,65,4)] cursor-pointer text-white py-3 rounded-2xl font-medium mb-4"
+                className="w-full mt-0.5 bg-primary-500 hover:bg-primary-600 cursor-pointer text-white py-3 rounded-2xl font-medium mb-4"
               >
                 Login
               </button>
@@ -124,22 +120,26 @@ export default function Login() {
             </div>
 
             <div className="flex justify-center">
-              <GoogleLogin className="cursor-pointer"
+              <GoogleLogin
+                className="cursor-pointers"
                 onSuccess={handleGoogleLoginSuccess}
                 onError={() => {
-                  console.log('Google login failed');
+                  console.log("Google login failed");
                 }}
               />
             </div>
 
-            <div className='flex items-center justify-center mt-2 text-md'>
-              <p>Don't have an account? <a href="/signup" className="text-blue-500 hover:underline ">Sign up</a></p>
+            <div className="flex items-center justify-center mt-2 text-md">
+              <p>
+                Don't have an account?{" "}
+                <a href="/signup" className="text-blue-500 hover:underline ">
+                  Sign up
+                </a>
+              </p>
             </div>
-
           </div>
         </div>
-
       </div>
     </>
-  )
+  );
 }
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,17 +1,16 @@
-import React, { useState } from 'react'
-import Navbar from '../components/Navbar'
-import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from 'jwt-decode'
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
-import api from "../api/axios"
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import api from "../api/axios";
 
 export default function Signup() {
-
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
@@ -25,24 +24,25 @@ export default function Signup() {
         username,
         email,
         password,
-        confirmPassword
+        confirmPassword,
       });
 
       console.log("Signup response:", response.data);
-      alert('Signup successful! Please log in.');
-      navigate('/login');
-    } 
-    catch (error) {
+      alert("Signup successful! Please log in.");
+      navigate("/login");
+    } catch (error) {
       console.error("Signup failed:", error);
-      alert(error.response?.data?.message || "Signup failed. Please try again.");
+      alert(
+        error.response?.data?.message || "Signup failed. Please try again.",
+      );
     }
-  }
+  };
 
   // GOOGLE SIGNUP
   const handleGoogleLoginSuccess = async (credentialResponse) => {
     try {
       const response = await api.post("/login/google", {
-        token: credentialResponse.credential
+        token: credentialResponse.credential,
       });
 
       console.log("Backend response:", response.data);
@@ -53,27 +53,22 @@ export default function Signup() {
 
       // alert(`Welcome, ${user.name}!`);
       navigate("/");
-
     } catch (error) {
       console.error("Google login failed at backend:", error);
       alert("Google login failed. Please try again.");
     }
-  }
+  };
 
   return (
     <>
-      <div className="min-h-screen bg-gray-300">
-
+      <div className="min-h-screen bg-background ">
         {/* Navbar */}
         <Navbar title="Log in" />
 
         {/* Centered Card */}
         <div className="py-5 flex justify-center items-center mt-10">
-          <div className="bg-[rgb(241,236,236)] w-100 rounded-2xl shadow-lg p-8">
-
-            <h2 className="text-2xl font-bold text-center mb-6">
-              Sign Up
-            </h2>
+          <div className="bg-background w-100 rounded-2xl shadow-lg p-8">
+            <h2 className="text-2xl font-bold text-center mb-6">Sign Up</h2>
 
             <form onSubmit={handleManualSignup}>
               {/* Username */}
@@ -113,9 +108,9 @@ export default function Signup() {
                 <button
                   type="button"
                   onClick={() => setShowPassword(!showPassword)}
-                  className='absolute right-3 top-10 cursor-pointer'
+                  className="absolute right-3 top-10 cursor-pointer"
                 >
-                  {showPassword ? <FaEye/>:<FaEyeSlash /> }
+                  {showPassword ? <FaEye /> : <FaEyeSlash />}
                 </button>
               </div>
 
@@ -132,16 +127,16 @@ export default function Signup() {
                 <button
                   type="button"
                   onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                  className='absolute right-3 top-10 cursor-pointer'
+                  className="absolute right-3 top-10 cursor-pointer"
                 >
-                  {showConfirmPassword ? <FaEye /> : <FaEyeSlash /> }
+                  {showConfirmPassword ? <FaEye /> : <FaEyeSlash />}
                 </button>
               </div>
 
               {/* SignUp Button */}
               <button
                 type="submit"
-                className="w-full mt-0.5 bg-[rgb(227,82,5)] hover:bg-[rgb(180,65,4)] cursor-pointer text-white flex justify-center py-3 rounded-2xl font-medium mb-4"
+                className="w-full mt-0.5 bg-primary-500 hover:bg-primary-600 cursor-pointer text-white flex justify-center py-3 rounded-2xl font-medium mb-4"
               >
                 Sign Up
               </button>
@@ -155,22 +150,26 @@ export default function Signup() {
             </div>
 
             <div className="flex justify-center">
-              <GoogleLogin className="cursor-pointer"
+              <GoogleLogin
+                className="cursor-pointer"
                 onSuccess={handleGoogleLoginSuccess}
                 onError={() => {
-                  console.log('Google login failed');
+                  console.log("Google login failed");
                 }}
               />
             </div>
 
-            <div className='flex items-center justify-center mt-2 text-md'>
-              <p>Already have an account? <a href="/login" className="text-blue-500 hover:underline ">Login</a></p>
+            <div className="flex items-center justify-center mt-2 text-md">
+              <p>
+                Already have an account?{" "}
+                <a href="/login" className="text-blue-500 hover:underline ">
+                  Login
+                </a>
+              </p>
             </div>
-
           </div>
         </div>
-
       </div>
     </>
-  )
+  );
 }
