2d95abe2a64357a262633132afe551413e51f5ed
Phyo Arkar Win <phyoarkar3730@gmail.com>
2026-02-28 02:56:26 +0700

    Added Creator in Ticket

--- backend/controllers/issueSubmission.controller.js
+++ backend/controllers/issueSubmission.controller.js
@@ -1,13 +1,15 @@
 import generateDraftTicket from "../services/ollama.service.js";
 import sendEmail from "../services/email/email.service.js";
+import User from "../models/user.js";
 
 const handleIssueSubmission = async (req, res) => {
     const { email, issue } = req.body;
+    let user = await User.findOne({ email });
     if (!issue) {
         return res.status(400).json({ error: "Missing message in request body" });
     }
     try {
-        await generateDraftTicket(email, issue);
+        await generateDraftTicket(email, issue, user);
         await sendEmail(email, issue);
         res.status(200).json({ message: "Email sent successfully" });
     } catch (error) {
