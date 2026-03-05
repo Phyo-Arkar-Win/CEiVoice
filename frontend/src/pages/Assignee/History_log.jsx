98a7cc8b150c4b2cfff4c9a8de8c6dc494e40a98
67011617_luu <67011617@kmitl.ac.th>
2026-03-05 22:23:47 +0700

    assignee_navbar

---
+++ frontend/src/pages/Assignee/History_log.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AssigneeNavbar from '@/components/AssigneeNavbar'
+
+export default function Assignee_Historylog() {
+  return (
+    <>
+    <div className='min-h-screen flex'>
+    <AssigneeNavbar></AssigneeNavbar>
+    <div className='m-6 text-2xl'> Sate Nyit Sayr </div>
+    </div>
+    </>
+  )
+}
