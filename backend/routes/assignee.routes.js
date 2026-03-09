56f4aae9ca362934942427b0a46530c2153df422
Leur <huangzule0118@gmail.com>
2026-03-10 05:35:26 +0700

    ﻿chore: update assginee routes

--- backend/routes/assignee.routes.js
+++ backend/routes/assignee.routes.js
@@ -1,12 +1,11 @@
 import { Router } from 'express';
 import { getAssigneeDashboardData } from '../controllers/dashboard.controller.js';
 import authController from '../controllers/auth.controller.js';
-import { ticketDetailsAsAdminOrAssignee, submitCommentAsAdminOrAssignee, saveAsAssignee } from '../controllers/ticket.controller.js';
+import { ticketDetailsAsAdminOrAssignee, submitCommentAsAdminOrAssignee } from '../controllers/ticket.controller.js';
 
 const router = Router();
 router.get('/dashboard', authController.protect, getAssigneeDashboardData);
 router.get('/ticketDetails/:id', ticketDetailsAsAdminOrAssignee);
 router.post('/submitComment', submitCommentAsAdminOrAssignee);
-router.post('/saveTicket', saveAsAssignee);
 
 export default router;  
