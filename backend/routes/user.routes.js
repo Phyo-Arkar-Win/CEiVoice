6957d2f37f78c06787b63ec2eaaf8371e513152f
Leur <huangzule0118@gmail.com>
2026-03-10 05:17:48 +0700

    ﻿chore: update user routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -5,7 +5,7 @@ import { ticketDetailsAsUser, submitCommentAsUser } from '../controllers/ticket.
 
 const router = Router();
 router.get('/tickets', getUserTickets);
-router.get('/ticketDetails/:id', authController.protect, ticketDetailsAsUser); //edtbyRomulus
-router.post('/submitComment', authController.protect, submitCommentAsUser); //edtbyRomulus
+router.get('/ticketDetails/:id', ticketDetailsAsUser);
+router.post('/submitComment', submitCommentAsUser);
 
 export default router;
