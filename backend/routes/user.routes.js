a8fea07f57fb2a722913825f6e0f62eaf8b5aa72
Aung Pyae Sone <101930261+Romulus004@users.noreply.github.com>
2026-03-11 01:59:19 +0700

    ﻿comment section works fine

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,10 +2,9 @@ import { Router } from 'express';
 import authController from '../controllers/auth.controller.js';
 import { getUserTickets } from '../controllers/dashboard.controller.js';
 import { ticketDetailsAsUser, submitCommentAsUser } from '../controllers/ticket.controller.js';
-import { auth } from 'google-auth-library';
 
 const router = Router();
-router.get('/tickets', authController.protect, getUserTickets);
+router.get('/tickets', getUserTickets);
 router.get('/ticketDetails/:id', authController.protect, ticketDetailsAsUser); //edtbyRomulus
 router.post('/submitComment', authController.protect, submitCommentAsUser); //edtbyRomulus
 
